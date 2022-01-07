c4ac4a63be6c76eee6b64a224556802fa205b450
JC-Null <johncanzo@icloud.com>
2022-01-07 02:55:01 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by John Canzoneri on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
