b3189d6b9fb91c794abea15f2ac0551193ccc913
JC-Null <johncanzo@icloud.com>
2022-01-08 15:52:52 -0500

    Upaded Homeview to get image and basic UI
    UI of homeview added - next steps are to add some graphics

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -14,16 +14,44 @@ struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            //MARK: - HEADER
+            
+            Spacer()
+            
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            //MARK: - CENTER
+            
+            Text("The time that leads to mastery is depedent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            //MARK: - FOOTER
+            Spacer()
             
             Button(action: {
                 
                 //some action
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+                    
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
+            
         } //: Vstack
     }
 }
