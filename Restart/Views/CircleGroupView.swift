07243ff5cfc6e8e70b962610f4a76577be96a274
JC-Null <johncanzo@icloud.com>
2022-01-09 11:29:35 -0500

    Creating multi-use views
    Created the white circle background that will be used in multiple screens

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,43 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by John Canzoneri on 1/8/22.
+//
+
+import SwiftUI
+//MARK: - PROPERTY
+
+
+
+//MARK: - BODY
+
+struct CircleGroupView: View {
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+
+    var body: some View {
+        ZStack{
+            Circle()
+                .stroke(.white.opacity(0.2), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(.white.opacity(0.2), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        }
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+        
+        
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+       
+        }
+    }
+}
